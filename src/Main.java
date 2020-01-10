45f6b06619715ccd555a61b469c448c435c8b8e2
Ekaterina2908 <NamestnikovaKatya@yandex.ru>
2020-01-10 23:36:22 +0600

    lab-4

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,14 @@ public class Main {
         int y = 5;
         int width = 10;
         int height = 2;
-        Rectangle rectangle = new Rectangle (x, y, width, height);
+      //  Rectangle rectangle = new Rectangle (x, y, width, height);
         rectangle.out (x, y, width, height);
 
         x = 34;
         y = 56;
         width = 35;
         height = 1;
-        Square square = new Square (x, y, width, height);
+       // Square square = new Square (x, y, width, height);
         square.out(x,  y, width, height);
 
     }
